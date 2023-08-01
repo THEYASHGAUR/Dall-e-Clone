4c8b77b5eb27a2d1718d7668f4086c2951073941
Yash Gaur <yashgaur2525@gmail.com>
2023-08-02 02:17:14 +0530

    updated backend urls

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dalle-clone-90w6.onrender.com//api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
